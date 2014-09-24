6088aaf3953458a9e0b220c2a6aa2dbf76cd9f8c
pascalc <pascal.chevrel@free.fr>
2014-09-24 16:17:12 +0200

    add a third column to devtools view

--- app/models/locale.php
+++ app/models/locale.php
@@ -1,16 +1,14 @@
 <?php
 namespace Transvision;
 
-$english = new Product('Firefox', 'en-US', 'aurora');
-$total_english  = count($english->excludeAccesskeys()->getStrings());
-$total_devtools = count($english->excludeAccesskeys()->getDevToolsStrings());
+$english = new Product('Firefox', 'en-US', 'aurora', false);
+
 $results = [];
 
 foreach (Project::getRepositoryLocales('aurora') as $locale) {
-    $target = new Product('Firefox', $locale, 'aurora');
-    $results[$locale]['total_missing'] = $total_english - count($target->excludeAccesskeys()->getStrings());
-    $results[$locale]['devtools_missing'] = $total_devtools - count($target->excludeAccesskeys()->getDevToolsStrings());
+    $target = new Product('Firefox', $locale, 'aurora', false);
+    $results[$locale]['total_missing']    = count($english->getStrings()) - count($target->getStrings());
+    $results[$locale]['devtools_missing'] = count($english->getDevToolsStrings()) - count($target->getDevToolsStrings());
 }
 
 unset($english, $target);
-
--- app/views/locale.php
+++ app/views/locale.php
@@ -3,6 +3,7 @@
         <th>Firefox Aurora</th>
         <th>Missing strings</th>
         <th>(in Devtools)</th>
+        <th>Missing strings <br>devtools excluded</th>
     </tr>
 
     <?php foreach($results as $k => $v): ?>
@@ -10,6 +11,7 @@
         <th><a href="#<?=$k?>"><?=$k?></a></th>
         <td style="text-align:right;"><?=$v['total_missing']?></td>
         <td style="text-align:right;"><?=$v['devtools_missing']?></td>
+        <td style="text-align:right;"><?=$v['total_missing'] - $v['devtools_missing']?></td>
     </tr>
     <?php endforeach; ?>
 
